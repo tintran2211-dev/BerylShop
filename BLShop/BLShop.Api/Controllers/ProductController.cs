230a5aa852af1cfcf1614aa553eac9f0c6b00688
unknown <tintran2211@gmail.com>
2022-08-15 23:49:30 +0700

    initials first

---
+++ BLShop/BLShop.Api/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using BLShop.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var product = new Product();
+            product.Id = 1;
+            product.Name = "IPhone13 XSMax";
+            product.Price = 30.5f;
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = new Product();
+            product.Id = id;
+            product.Name = "IPhone13 XSMax";
+            product.Price = 30.5f;
+
+            return Ok(product);
+        }
+    }
+}
---
+++ BLShop/BLShop.Domain/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLShop.Domain.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public long Ssn { get; set; }
+        public string Username { get; set; }
+
+
+        public ICollection<ContactDetail> ContactDetails { get; set; }
+        public Name Name { get; set; }
+    }
+}
